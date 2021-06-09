dd26dafc5a37b3d9cd7e36ccd52c49f8ba9e75a8
Caio <caio.matheus@everysystem.com.br>
2021-06-09 03:22:14 -0300

    commit testing

--- tests/Feature/CompanyRepositoryTest.php
+++ tests/Feature/CompanyRepositoryTest.php
@@ -44,4 +44,5 @@ it('can delete a company', function () {
     $company = $this->companyFactory->create();
     $response = $this->actingAs($this->user)->deleteJson("/api/companies/" . $company->id)->assertJson(['success' => 'true']);
     $response->assertStatus(200);
+    
 });
